dfb19c17863f7f3553de38ba0d057307c52f9527
Gustavo <tiratreco@gmail.com>
2020-12-18 20:42:26 -0300

    Add mergesort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from heapsort import heapSort
 from bubblesort import bubbleSort
 from insertionsort import insertionSort
+from mergesort import mergeSort
 
 vetoresAleatorios = open("vetoresAleatorios.txt", "r")
 vetores = []
@@ -16,6 +17,7 @@ i = 1
 
 #heapSort(vetores[i])
 #bubbleSort(vetores[i])
-insertionSort(vetores[i])
+#insertionSort(vetores[i])
+mergeSort(vetores[i])
 print(vetores[i])
     
